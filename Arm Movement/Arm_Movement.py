e6515ec1e181ce3eb123c9f2e23301235856fe77
gregferguson117 <35634808+gregferguson117@users.noreply.github.com>
2021-04-05 00:27:09 -0500

    Updated Arm Movement script
    Added more networking functionality.

--- Arm Movement/Arm_Movement.py
+++ Arm Movement/Arm_Movement.py
@@ -1,7 +1,13 @@
 import socket
 import json
 import numpy as np
+import time
+import threading
 
+
+
+systemStatus = "Offline"
+armPosition = "Unknown"
 
 HOST = '127.0.0.1'
 PORT = 6000
@@ -13,39 +19,127 @@ PORT = 6000
 #Ready to add other functions for arm movement
 
 
-def getTCPValues(socket):
+
+
+#Tells server ready for TCP values
+def TCPwrapper(conn):
+    global systemStatus
+    global armPosition
+    
+    if(systemStatus == "Online"):
+        if(armPosition == "Home"):
+            
+            jsonResult = {"first":"Client 1", "second": "Ready for TCP Values"}
+            send(conn, jsonResult)
+    
+
+def getTCPValues(jsonReceived):
     TCPvalues = np.zeros(6)
     
+    #See if there is a better way to do this
+   
+    TCPvalues[0] = int(jsonReceived["second"])
+    TCPvalues[1] = int(jsonReceived["third"])
+    TCPvalues[2] = int(jsonReceived["fourth"])
+    TCPvalues[3] = int(jsonReceived["fifth"])
+    TCPvalues[4] = int(jsonReceived["sixth"])
+    TCPvalues[5] = int(jsonReceived["seventh"])
     
-    jsonResult = {"first":"Client 1", "second":"Requesting TCP Values"}
-    jsonResult = json.dumps(jsonResult)
-    socket.send(bytes(jsonResult, encoding="utf-8"))
+    #Call arm movement here
 
-    jsonReceived = socket.recv(1024)
-    jsonReceived = json.loads(jsonReceived.decode("utf-8"))
+
+    
+
+def send(conn, data):
+        
+    jsonResult = json.dumps(data)
+    conn.send(bytes(jsonResult, encoding="utf-8"))
+    
+    
+def moniterUserInput(conn):
+    global systemStatus 
+    
+    while(True):
+        jsonReceived = conn.recv(1024)
+        jsonReceived = json.loads(jsonReceived.decode("utf-8"))
+        
+        #Add commands for this script
+        #System Pasue, shutdown, velocity change, etc. 
+        
+        
+        if(jsonReceived["first"] == "System Started"):
+            print("System online")
+            systemStatus = "Online"
+            
+        elif(jsonReceived["first"] == "Go into Standby"):
+            print("System in standby")
+            systemStatus = "Standby"
+            
+        elif(jsonReceived["first"] == "Shut Down System"):
+            print("System offline")
+            systemStatus = "Offline"
+          
+            
+            #Move arm to home position
+              
+        elif(jsonReceived["first"] == "Velocity Change"):
+            print("Velocity change")
+            print(jsonReceived["second"])
+            #Call function to change arm velocity
+        
+        
+        elif(jsonReceived["first"] == "TCP Values"):
+            print("TCP Values")
+            
+            #If the system isnt in standby or shutdown, allowed to move the arm
+            if(systemStatus == "Online"):
+                print()
+                
+                
+                #Request TCP values
+                #Move arm to pick up parcel
+                #Sort parcel
+                #Move to home position
+                #Repeat
+      
+            
+def main():
+ 
+    connected = False
+    
+    conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    
+    
+    while not(connected):
+    
+        try:
+            conn.connect((HOST, PORT))
+            connected = True
+        except Exception as e:
+            print(e)
+        finally:
+            time.sleep(1)
+    
+    
+    
+    
+    t1 = threading.Thread(target = moniterUserInput, args = (conn, ))
+           
+    t1.start()
+                  
+    #Move arm to home position 
+    
+    
+    #May put this in a while loop, run forever
+    #Not sure yet. 
+    #TCPwrapper()
+    
+    
+    t1.join()
     
    
-    #See if there is a better way to do this
-    TCPvalues[0] = int(jsonReceived["first"])
-    TCPvalues[1] = int(jsonReceived["second"])
-    TCPvalues[2] = int(jsonReceived["third"])
-    TCPvalues[3] = int(jsonReceived["fourth"])
-    TCPvalues[4] = int(jsonReceived["fifth"])
-    TCPvalues[5] = int(jsonReceived["sixth"])
-    
-    return TCPvalues
-
 
    
 if __name__ == "__main__":
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    
-    s.connect((HOST, PORT))
-    
-    
-    #Call this function as needed. 
-    #My suggestion would be to set the arm to the home location and then request new TCP values. 
-    TCPValues = getTCPValues(s)
-    
-    
+    main()
         
