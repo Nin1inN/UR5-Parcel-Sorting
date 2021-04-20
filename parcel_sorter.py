3f97fdddd8aafe8f25197092b29eeb793057dd64
Donny Pham <Donny1Pham@yahoo.com>
2021-04-20 16:26:41 -0500

    Socket Integration

--- parcel_sorter.py
+++ parcel_sorter.py
@@ -4,25 +4,51 @@ The main file for the parcel sorter
 """
 import yolov5_Interface as yolov5
 import realsense_depth as rs
-import cv2
+from server import Server
 
 
 if __name__ == '__main__':
-    #Initialize Object Detection Model
-    model, objects, obj_colors = yolov5.create_model('weight_v1.pt')
-    
-    #Initialize Intel Realsense Sensor
-    sensor = rs.DepthCamera()
-    
-    #
-    while True:
-        ret, depth_frame, color_frame = sensor.get_frames()
-        
-        #Note model was trained on images that are sized 192x192 
-        status, depth, bounds, img = yolov5.detect(model, color_frame, depth_frame, 192, objects, obj_colors)
-            
-        # Show images
-        cv2.namedWindow('RealSense', cv2.WINDOW_AUTOSIZE)
-        cv2.imshow('RealSense', img)
-        cv2.waitKey(1)
+    HOST = '127.0.0.1'
+    PORT = 6000
 
+    """
+    Initialization
+    """
+    server = Server(HOST, PORT)
+    server.setup()
+    model, objects, obj_colors = yolov5.create_model('weight_v1.pt')
+    sensor = rs.DepthCamera()
+
+
+    while True:
+        """
+        Deal with communication events and block only for a second
+        """
+        json = ''
+        events = server.sel.select(timeout=1)
+        for key, mask in events:
+            if key.data is None:
+                server.accept_conn(key.fileobj)
+            else:
+                json = server.service_conn(key, mask)
+
+
+        #json will contain our messages obtained from either clients ;)
+
+        """
+        Process vision input
+        """
+        ret, depth_frame, color_frame = sensor.get_frames()
+
+        #Note model was trained on images that are sized 192x192
+        status, depth, bounds, frame = yolov5.detect(model, color_frame, depth_frame, 192, objects, obj_colors)
+
+        server.current_frame = frame
+
+
+        """
+        DO SOMETHING WITH THE INFORMATION GREG
+        Also you can do Server.send_to_arm( json/dictionary ) to send directly to arm 
+        """
+
+        
--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 """
-Used as a self manage interface for communication to the client.
+Used as a network interface for communication to the clients.
 
 Derived from https://realpython.com/python-sockets/#handling-multiple-connections
 """
@@ -9,147 +9,121 @@ import threading
 import selectors
 import types
 import json
+import numpy as np
 
-class Server: 
+"""
+Frame is a numpy array
+Server
+frame = frame.flatten()
+data = frame.tostring()
+
+Client
+frame = numpy.fromtstring(data, dtype=numpy.uint8)
+frame = numpy.reshape(frame, (640,480,3))
+"""
+
+class Server:
     def __init__(server, ip, port):
         server.ip = ip
         server.port = port
-        server.sockets = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        server.pending_msgs = []
-        server.threads = []
+        server.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server.clients = {}
+        server.stream_thread = ''
         server.sel = selectors.DefaultSelector()
-        server.state = 'Offline'
-        
-        
-    def start(server):
+        server.current_frame = np.zeros((640, 480), dtype=int)
+
+
+    def setup(server):
         #Initiate socket communications
         server.socket.bind((server.ip, server.port))
         server.socket.listen(5)
         print('Listening on', (server.ip, server.port))
-        
-        #Prep for multiple clients 
+
+        #Prep for multiple socket clients
         server.socket.setblocking(False)
         server.sel.register(server.socket, selectors.EVENT_READ, data=None)
-        
-        #Process connections and communications
-        while True:
-            events = server.sel.select(timeout=None)
-            for key, mask in events:
-                if key.data is None:
-                    server.accept_client(key.fileobj)
-                else:
-                    server.service_client(key, mask)
-                    
-    
-    def accept_client(server):
+
+
+    def accept_conn(server, sock):
         #accept the client
-        conn, addr = server.socket.accept()
+        conn, addr = sock.accept()
         print('Accepted connection', addr)
         conn.setblocking(False)
-        
-        server.clients.append(conn)
-        
+
         #Update the selector
         data = types.SimpleNamespace(addr=addr, inb=b'', outb=b'')
         events = selectors.EVENT_READ | selectors.EVENT_WRITE
+
+        #Add new client in the selector
         server.sel.register(conn, events, data=data)
-    
-    
-    def service_client(server, key, mask):
-        client_sock = key.fileobj
+
+
+    def service_conn(server, key, mask):
+        conn = key.fileobj
         data = key.data
-        
+        msg = ''
+
+        #Obtain message from connections
         if mask & selectors.EVENT_READ:
-            recv_data = client_sock.recv(1024)
+            recv_data = conn.recv(1024)
+            
             if recv_data:
                 data.outb += recv_data
-                
-                #Process json commands 
+
                 try:
                     jsonReceived = json.loads(recv_data.decode("utf-u"))
-                    if(jsonReceived['first'] == 'Client 1'):
-                        if(jsonReceived['second'] == 'Ready for TCP Values'):
-                            readyForTCPValues = 'Yes'
-                            
-                    elif(jsonReceived['first'] == 'Client 2'):
-                        if(jsonReceived['second'] == 'Start Up System'):
-                            if(server.state == 'Offline'):
-                                jsonResult = {'first':'System Started'}
-                                jsonResult = json.dumps(jsonResult)
-                                client_sock.send(bytes(jsonResult, encoding='utf-8'))
-                                
-                                server.state = 'Online'
-                                
-                                #t1 = threading.Thread(target = videoStream, args=(client_sock))
-                                #t1.start()
-                                
-                                print('Starting up')
-                                
-                        elif(jsonReceived['second'] == 'Resume System'):
-                            jsonResult = {'first':'System Resume'}
-                            jsonResult = json.dumps(jsonResult)
-                            client_sock.send(bytes(jsonResult, encoding='utf-8'))
-                                
-                            server.state = 'Online'
-                            
-                            print('System Resuming')
-                                
-                        elif(jsonReceived['second'] == 'Pause System'):
-                            jsonResult = {'first':'Go into Standby'}
-                            jsonResult = json.dumps(jsonResult)
-                            client_sock.send(bytes(jsonResult, encoding='utf-8'))
-                                
-                            server.state = 'Standby'
-                            
-                            print('System going into standby')
-                            
-                        elif(jsonReceived['second'] == 'Drop Location'):
-                            if(server.state == 'Online'):
-                                jsonResult = {'first':'Drop Location', 'second': jsonReceived["third"], "third": jsonReceived["fourth"], "fourth": jsonReceived["fifth"], "fifth": jsonReceived["sixth"], "sixth": jsonReceived["seventh"], "seventh": jsonReceived["eight"]}
-                                
-                                print(jsonResult)
-                                
-                        elif(jsonReceived['second'] == 'Set Velocity'):
-                            if(server.state == 'Online'):
-                                jsonResult = {'first':'Velocity Change', 'second': jsonReceived['third']}
-                                jsonResult = json.dumps(jsonResult)
-                                client_sock.send(bytes(jsonResult, encoding='utf-8'))
-                                
-                                
-                        elif(jsonReceived['second'] == 'Shut Down System'):
-                            server.state = 'Offline'
-                            
-                            #Client 0 for testing, client 1 for final version
+                    msg = jsonReceived
+                    
+                    #Check for ARM 
+                    if(jsonReceived['first'] == 'ARM'):
+                        server.clients['ARM'] = conn
 
-                            #Disconnect socket for video feed here
-                            try:
-                                client_sock.close()
-                                #VidGearServer.close()
-                                #stream.release()
-                                #temp = clients[0]
-                                #Keeping the first client (Arm Movement)
-                                #clients = []
-                                #clients.append(temp)
-            
-                            except Exception as e:
-                                pass
-                                #logging.error(e)
-                            
-                            finally:
-                                return
+                    #Check for CONTROL
+                    elif(jsonReceived['first'] == 'CONTROL'):
+                        server.clients['CONTROL'] = conn
+                        
+                        if (server.stream_thread == ''):
+                            server.stream_thread = threading.Thread(target = server.stream_current_frame, args = (server, conn))
+                            server.stream_thread.start()
                 except:
                     pass
-                
+
             else:
                 print('closing connection to', data.addr)
-                server.sel.unregister(client_sock)
-                client_sock.close()
-                
+                server.sel.unregister(conn)
+                conn.close()
+            
+
+        #Echos message received
         if mask & selectors.EVENT_WRITE:
             if data.outb:
                 print('echoing', repr(data.outb), 'to', data.addr)
-                sent = client_sock.send(data.outb)
+                sent = conn.send(data.outb)
                 data.outb = data.outb[sent:]
         
-        
-                    
+        return msg
+
+
+    def send_to_arm(server, data):
+        out = json.dumps(data)
+        server.clients['ARM'].send(bytes(out, encoding='utf-8'))
+
+
+    def send_to_control(server, data):
+        out = json.dumps(data)
+        server.clients['CONTROL'].send(bytes(out, encoding='utf-8'))
+
+
+    #Used to get visual feedback from vision systems
+    def stream_current_frame(server, conn):
+        try:
+            while True:
+                frame = server.current_frame.flatten()
+                data = frame.tostring()
+
+                out = { "Stream": data }
+                out = json.dumps(out)
+
+                conn.send(bytes(out, encoding="utf-8"))
+        finally:
+            print('Video Stream Ended')
